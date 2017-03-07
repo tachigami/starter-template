244c0b821c677af5d974c0150d8208cdbd3c32c1
Roman <bronner888@gmail.com>
2017-03-07 15:50:35 +0300

    Первый коммит

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    concat      = require('gulp-concat'),
+    connectPHP  = require('gulp-connect-php'),
+    cssnano     = require('gulp-cssnano'),
+    uglify      = require('gulp-uglifyjs'),
+    del         = require('del');
+
+const paths = {
+    html: ['public/*.php'],
+    sass: ['assets/sass/**/*'],
+    js: ['assets/js/**/*.js']
+}
+
+const vendor = {
+    css: [
+        'libs/bootstrap/dist/css/bootstrap.min.css'
+    ],
+    js: [
+        'libs/jquery/dist/jquery.min.js',
+        'libs/bootstrap/dist/js/bootstrap.min.js'
+    ]
+}
+
+gulp.task('build', ['clean', 'vendor:css', 'vendor:js', 'assets:sass', 'assets:js'])
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch(paths.sass, ['assets:sass']);
+    gulp.watch(paths.js, ['assets:js']);
+    gulp.watch(paths.html, ['html']);
+});
+
+gulp.task('live', ['php', 'browser-sync', 'watch']);
+
+gulp.task('html', function() {
+    return gulp.src(paths.html)
+        .pipe(reload({stream: true}))
+})
+
+gulp.task('assets:sass', function () {
+    return gulp.src('assets/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/css'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('assets:js', function() {
+    return gulp.src('assets/js/*.js')
+        .pipe(gulp.dest('public/js'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('vendor:js', function() {
+    return gulp.src(vendor.js)
+        .pipe(concat('vendor.min.js'))
+	    .pipe(uglify())
+	    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('vendor:css', function() {
+    return gulp.src(vendor.css)
+        .pipe(concat('vendor.min.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('clean', function() {
+    return del.sync([
+        'public/js/*',
+        'public/css/*'
+    ]);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: '127.0.0.1:8888'
+    });
+});
+
+gulp.task('php', function(){
+  connectPHP.server({ base: 'public', keepalive:true, hostname: 'localhost', port:8888, open: false});
+});
+
+gulp.task('default', ['build'])
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="stylesheet" href="/css/vendor.min.css" />
+        <link rel="stylesheet" href="/css/app.css" />
+        <title>Template</title>
+    </head>
+    <body>
+        <h1>Hello <?='PHP'?>!</h1>
+    </body>
+
+    <script src="/js/vendor.min.js"></script>
+    <script src="/js/app.js"></script>
+</html>
