1d7e9f67555c2c553982a5ea72e1ab2d5e2d7464
Roman <bronner888@gmail.com>
2017-03-21 12:17:39 +0300

    Мелкие исправления путей

--- public/index.php
+++ public/index.php
@@ -2,15 +2,15 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="shortcut icon" href="/img/favicon.ico" />
-        <link rel="stylesheet" href="/css/vendor.min.css" />
-        <link rel="stylesheet" href="/css/app.css" />
+        <link rel="shortcut icon" href="img/favicon.ico" />
+        <link rel="stylesheet" href="css/vendor.min.css" />
+        <link rel="stylesheet" href="css/app.css" />
         <title>Template</title>
     </head>
     <body>
         <h1>Hello <?='PHP'?>!</h1>
     </body>
 
-    <script src="/js/vendor.min.js"></script>
-    <script src="/js/app.js"></script>
+    <script src="js/vendor.min.js"></script>
+    <script src="js/app.js"></script>
 </html>
