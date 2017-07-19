b222ecab702862876090e30007f687d5c7acaf12
Roman <bronner888@gmail.com>
2017-07-19 16:43:08 +0300

    (╯°□°）╯︵ ┻━┻

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,264 @@
-'use strict'
+'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
+var _if = require('gulp-if');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var concat = require('gulp-concat');
-var connectPHP = require('gulp-connect-php');
-var cssnano = require('gulp-cssnano');
-var uglify = require('gulp-uglifyjs');
+var uglify = require('gulp-uglify');
+var cleanCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
 var del = require('del');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var cache = require('gulp-cache');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var connectPhp = require('gulp-connect-php');
+var fileinclude = require('gulp-file-include');
 
-var paths = {
-    html: ['public/*.php', 'public/*.html'],
-    sass: ['assets/sass/**/*'],
-    js: ['assets/js/**/*.js']
+var options = {
+    useTemplates: true,
+    production: false,
+    openBrowser: true,
+    notifications: false,
+    phpProxyHost: '127.0.0.1',
+    phpProxyPort: 8888,
+    srcDir: 'assets',
+    destDir: 'public'
 };
 
-var vendor = {
+/**
+ * Содержит пути к библиотекам, установленных с помощью Yarn и компилирующихся в vendor.min.(css|js)
+ */
+var libs = {
+    sass: [
+        './node_modules/bootstrap-sass-grid/sass/bootstrap-sass-grid.scss',
+    ],
     css: [
-        'libs/bootstrap/dist/css/bootstrap.min.css'
+        './node_modules/normalize.css/normalize.css',
     ],
     js: [
-        'libs/jquery/dist/jquery.min.js',
-        'libs/bootstrap/dist/js/bootstrap.min.js'
+        './node_modules/jquery/dist/jquery.min.js',
     ]
 };
 
-gulp.task('build', ['clean', 'vendor:css', 'vendor:js', 'assets:sass', 'assets:js']);
+var paths = {
+    sass: {
+        src: options.srcDir + '/sass',
+        dest: options.destDir + '/css',
+        mask: '/**/*.*'
+    },
+    js: {
+        src: options.srcDir + '/js',
+        dest: options.destDir + '/js',
+        mask: '/**/*.*'
+    },
+    img: {
+        src: options.srcDir + '/img',
+        dest: options.destDir + '/img',
+        mask: '/**/*.*'
+    },
+    html: {
+        src: options.srcDir + '/templates',
+        dest: options.destDir + '/',
+        mask: '/**/[^_]*.+(html|php)'
+    },
+    fonts: {
+        src: options.srcDir + '/fonts',
+        dest: options.destDir + '/',
+        mask: '/**/*.*'
+    },
+    watch: {
+        sass: options.srcDir + '/sass/**/*.+(sass|scss)',
+        js: options.srcDir + '/js/**/*.js',
+        img: options.srcDir + '/img/**/*.+(png|jpg|jpeg|gif|svg)',
+        html: options.srcDir + '/**/*.+(html|php)'
+    },
+    vendor: {
+        css: 'vendor.min.css',
+        js: 'vendor.min.js'
+    }
+};
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch(paths.sass, ['assets:sass']);
-    gulp.watch(paths.js, ['assets:js']);
-    gulp.watch(paths.html, ['html']);
-});
+var handleError = function (err) {
+    notify({
+        title: 'Gulp Task Error',
+        message: '!!!ERROR!!! Check the console.'
+    }).write(err);
+    console.log(err.toString());
+    this.emit('end');
+};
 
-gulp.task('live', ['watch'], function () {
-    connectPHP.server({base: './public', keepalive: true, hostname: '127.0.0.1', port: 8888, open: false});
-    browserSync.init({
-        proxy: '127.0.0.1:8888',
-        notify: false,
-        open: false
-    });
-});
+var handleWatchEvent = function (event, filePath, description) {
+    if (event.type === 'deleted') {
+        var filePathFromSrc = path.relative(path.resolve(filePath.src), event.path);
+        var destFilePath = path.resolve(filePath.dest, filePathFromSrc);
+        console.log(destFilePath);
+        del.sync(destFilePath);
+    }
+    notify({
+        title: 'Gulp Task Complete',
+        message: description + ' has been compiled'
+    }).write('');
+};
 
-gulp.task('live:html', ['watch'], function () {
-    browserSync.init({
-        server: {
-            baseDir: './public'
-        },
-        notify: false,
-        open: false
-    });
-});
-
-gulp.task('html', function () {
-    return gulp.src(paths.html)
-        .pipe(reload({stream: true}));
+gulp.task('assets:html', function () {
+    if (options.useTemplates) {
+        return gulp.src(paths.html.src + paths.html.mask)
+            .pipe(plumber({errorHandle: handleError}))
+            .pipe(fileinclude())
+            .pipe(gulp.dest(paths.html.dest))
+            .pipe(browserSync.stream({once: true}))
+    }
 });
 
 gulp.task('assets:sass', function () {
-    return gulp.src('assets/sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
+    return gulp.src(paths.sass.src + paths.sass.mask)
+        .pipe(plumber({errorHandle: handleError}))
+        .pipe(_if(!options.production, sourcemaps.init()))
+        .pipe(sass().on('error', handleError))
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-        .pipe(gulp.dest('public/css'))
-        .pipe(reload({stream: true}));
+        .pipe(cleanCss())
+        .pipe(_if(!options.production, sourcemaps.write()))
+        .pipe(gulp.dest(paths.sass.dest))
+        .pipe(browserSync.stream({once: true}))
 });
 
 gulp.task('assets:js', function () {
-    return gulp.src('assets/js/*.js')
-        .pipe(gulp.dest('public/js'))
-        .pipe(reload({stream: true}));
+    return gulp.src(paths.js.src + paths.js.mask)
+        .pipe(plumber({errorHandle: handleError}))
+        .pipe(_if(!options.production, sourcemaps.init()))
+        .pipe(fileinclude())
+        .pipe(uglify())
+        .pipe(_if(!options.production, sourcemaps.write()))
+        .pipe(gulp.dest(paths.js.dest))
+        .pipe(browserSync.stream({once: true}))
+});
+
+gulp.task('assets:img', function () {
+    return gulp.src(paths.img.src + paths.img.mask)
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPluggins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest(paths.img.dest))
+        .pipe(browserSync.stream({once: true}))
+});
+
+gulp.task('assets:fonts', function () {
+    return gulp.src(paths.fonts.src + paths.fonts.mask)
+        .pipe(gulp.dest(paths.fonts.dest));
 });
 
 gulp.task('vendor:js', function () {
-    return gulp.src(vendor.js)
-        .pipe(concat('vendor.min.js'))
+    return gulp.src(libs.js)
+        .pipe(plumber({errorHandle: handleError}))
+        .pipe(_if(!options.production, sourcemaps.init()))
+        .pipe(concat(paths.vendor.js))
         .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+        .pipe(_if(!options.production, sourcemaps.write()))
+        .pipe(gulp.dest(paths.js.dest));
 });
 
 gulp.task('vendor:css', function () {
-    return gulp.src(vendor.css)
-        .pipe(concat('vendor.min.css'))
-        .pipe(cssnano())
-        .pipe(gulp.dest('public/css'));
+    return gulp.src(libs.css)
+        .pipe(plumber({errorHandle: handleError}))
+        .pipe(_if(!options.production, sourcemaps.init()))
+        .pipe(concat(paths.vendor.css))
+        .pipe(cleanCss())
+        .pipe(_if(!options.production, sourcemaps.write()))
+        .pipe(gulp.dest(paths.sass.dest))
 });
 
-gulp.task('clean', function () {
-    return del.sync([
-        'public/js/*',
-        'public/css/*'
-    ]);
+gulp.task('vendor:sass', function () {
+    return gulp.src(libs.sass)
+        .pipe(plumber({errorHandle: handleError}))
+        .pipe(sass().on('error', handleError))
+        .pipe(concat(paths.vendor.css))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+        .pipe(cleanCss())
+        .pipe(gulp.dest(paths.sass.dest))
+});
+
+gulp.task('clean', ['clear'], function (cb) {
+
+    if (options.useTemplates) {
+        del.sync([options.destDir + '/**/*', '!' + options.destDir + '/.gitkeep'], {dot: true}, cb);
+    } else {
+        return del.sync([
+            paths.js.dest,
+            paths.sass.dest,
+            paths.img.dest,
+            paths.fonts.dest
+        ]);
+    }
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch(paths.watch.sass, ['assets:sass']).on('change', function (event) {
+        handleWatchEvent(event, paths.sass, 'SASS')
+    });
+    gulp.watch(paths.watch.js, ['assets:js']).on('change', function (event) {
+        handleWatchEvent(event, paths.js, 'JS')
+    });
+    gulp.watch(paths.watch.img, ['assets:img']).on('change', function (event) {
+        handleWatchEvent(event, paths.img, 'IMG')
+    });
+    gulp.watch(paths.watch.img, ['assets:fonts']).on('change', function (event) {
+        handleWatchEvent(event, paths.fonts, 'FONTS')
+    });
+    if (options.useTemplates) {
+        gulp.watch(paths.watch.html, ['assets:html']).on('change', function (event) {
+            handleWatchEvent(event, paths.html, 'HTML')
+        });
+    }
+});
+
+gulp.task('php', ['watch'], function () {
+    connectPhp.server({base: options.destDir, keepalive: true, hostname: options.phpProxyHost, phpProxyPort: options.phpProxyPort, open: false});
+    browserSync.init({
+        proxy: options.phpProxyHost + ':' + options.phpProxyPort,
+        notify: options.notifications,
+        open: options.openBrowser
+    });
+});
+
+gulp.task('live', ['watch'], function () {
+    browserSync.init({
+        server: {
+            baseDir: options.destDir
+        },
+        notify: options.notifications,
+        open: options.openBrowser
+    });
+});
+
+gulp.task('build', [
+    'clean',
+    'vendor:sass',
+    'vendor:css',
+    'vendor:js',
+    'assets:sass',
+    'assets:js',
+    'assets:img',
+    'assets:html',
+    'assets:fonts'
+]);
+
+gulp.task('production', function () {
+    options.production = true;
+    gulp.run('build')
 });
 
 gulp.task('default', ['build']);
